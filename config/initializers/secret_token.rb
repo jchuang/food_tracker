f890cc29498b8de8e1056b7218eab21ade9f1d42
Janet Chuang <janet.chuang@gmail.com>
2013-12-30 20:51:12 -0500

    first commit, Rails 4.0.2 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FoodTracker::Application.config.secret_key_base = '1b89976d11eb898984840a91f935ec030bd25d2e72f506681b5d88a97bd5e56cc5b8a3ca6fc30f319adb94e3867a34d6e24dcb78e2acf570bb036e6df0e1540a'
